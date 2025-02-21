a21be3037bb16f18b336a69e9b1fc47dfb85b40e
Daembius <julienmayard@me.com>
2025-02-21 18:36:06 +0700

    Removed from server.js middleware to serve static from dist

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import multer from 'multer'
 import { createServer } from 'http'
-import path from 'path' // Import the path module here
+// import path from 'path' // Import the path module here
 import metadataRoutes from './routes/metadata.js'
 
 // Configure environment variables
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000
 
 // Middleware
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'dist'))) // Serve static files from dist
+// app.use(express.static(path.join(__dirname, 'dist'))) // Serve static files from dist
 
 // File upload configuration
 const upload = multer({
