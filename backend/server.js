9c18b2dab93f7b27f90652ce5ca29e2a1f337238
Daembius <julienmayard@me.com>
2025-02-21 17:52:30 +0700

    Added UI build to package.json and middleware to server.js for deployment

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 5000
 
 // Middleware
 app.use(cors())
+app.use(express.static(path.join(__dirname, 'dist'))) // Serve static files from dist
 
 // File upload configuration
 const upload = multer({
